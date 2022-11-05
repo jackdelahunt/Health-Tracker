68e9c20219ee3d48bda10ad8ce362c4e19657fb7
Jack Delahunt <45426048+jackdelahunt@users.noreply.github.com>
2022-11-05 20:30:17 +0000

    feat: added swagger doc generation

--- src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
+++ src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
@@ -5,58 +5,102 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import ie.setu.domain.User
 import ie.setu.domain.repository.UserDAO
 import io.javalin.http.Context
+import io.javalin.plugin.openapi.annotations.*
+
 
 object HealthTrackerController {
 
     private val userDAO = UserDAO()
 
-    fun getAllUser(ctx: Context) {
-        println("GET all users")
+    @OpenApi(
+        summary = "Get all users",
+        operationId = "getAllUsers",
+        tags = ["User"],
+        path = "/api/users",
+        method = HttpMethod.GET,
+        responses = [OpenApiResponse("200", [OpenApiContent(Array<User>::class)])]
+    )
+    fun getAllUsers(ctx: Context) {
         ctx.json(userDAO.getAll())
     }
 
+    @OpenApi(
+        summary = "Get user by ID",
+        operationId = "getUserById",
+        tags = ["User"],
+        path = "/api/users/{user-id}",
+        method = HttpMethod.GET,
+        pathParams = [OpenApiParam("user-id", Int::class, "The user ID")],
+        responses  = [OpenApiResponse("200", [OpenApiContent(User::class)])]
+    )
     fun getUserByUserId(ctx: Context) {
-        println("GET user by id")
-
         val user = userDAO.findById(ctx.pathParam("user-id").toInt())
-
-        if(user != null) {
+        if (user != null) {
             ctx.json(user)
         }
     }
 
+    @OpenApi(
+        summary = "Add User",
+        operationId = "addUser",
+        tags = ["User"],
+        path = "/api/users",
+        method = HttpMethod.POST,
+        pathParams = [OpenApiParam("user-id", Int::class, "The user ID")],
+        responses  = [OpenApiResponse("200")]
+    )
     fun addUser(ctx: Context) {
-        println("POST create user")
-
         val mapper = jacksonObjectMapper()
         val user = mapper.readValue<User>(ctx.body())
         userDAO.save(user)
         ctx.json(user)
     }
 
+    @OpenApi(
+        summary = "Get user by Email",
+        operationId = "getUserByEmail",
+        tags = ["User"],
+        path = "/api/users/email/{email}",
+        method = HttpMethod.GET,
+        pathParams = [OpenApiParam("email", Int::class, "The user email")],
+        responses  = [OpenApiResponse("200", [OpenApiContent(User::class)])]
+    )
     fun getUserByEmail(ctx: Context) {
-        println("GET user by email")
-
-        val user = userDAO.findByEmail(ctx.pathParam("email").toString())
-
-        if(user != null) {
+        val user = userDAO.findByEmail(ctx.pathParam("email"))
+        if (user != null) {
             ctx.json(user)
         }
     }
 
+    @OpenApi(
+        summary = "Delete user by ID",
+        operationId = "deleteUserById",
+        tags = ["User"],
+        path = "/api/users/{user-id}",
+        method = HttpMethod.DELETE,
+        pathParams = [OpenApiParam("user-id", Int::class, "The user ID")],
+        responses  = [OpenApiResponse("204")]
+    )
     fun deleteUser(ctx: Context){
-        println("DELETE user by id")
-
         userDAO.delete(ctx.pathParam("user-id").toInt())
     }
 
+    @OpenApi(
+        summary = "Update user by ID",
+        operationId = "updateUserById",
+        tags = ["User"],
+        path = "/api/users/{user-id}",
+        method = HttpMethod.PATCH,
+        pathParams = [OpenApiParam("user-id", Int::class, "The user ID")],
+        responses  = [OpenApiResponse("204")]
+    )
     fun updateUser(ctx: Context){
         println("PATCH user by id")
 
         val id = ctx.pathParam("user-id").toInt()
         val mapper = jacksonObjectMapper()
         val user = mapper.readValue<User>(ctx.body())
-        userDAO.updateUser(id, user)
+        userDAO.update(id, user)
     }
 
 }
